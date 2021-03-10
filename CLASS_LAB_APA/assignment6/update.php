b2a7c6a2df61a4c17c401098178cfffd7ff343b6
tmehersairam <80393781+tmehersairam@users.noreply.github.com>
2021-03-10 16:27:33 +0530

    added some code

--- CLASS_LAB_APA/assignment6/update.php
+++ CLASS_LAB_APA/assignment6/update.php
@@ -4,13 +4,19 @@ include("../assignment5/config.php");
 $id = $_POST['id'];
 $last_name = $_POST['last_name'];
 $phone = $_POST['phone'];
-
-$query1 = "update students set last_name = '$last_name' , phone = '$phone' where id = $id" ;
-$exec1 = mysqli_query($con, $query1) or die(mysqli_error($con));
-if($exec1)
+if($id !== '' || $last_name !== '' || $phone !== '')
 {
-    header("location: index.php");
+    // we will update on the basis of id
+    $query1 = "update students set last_name = '$last_name' , phone = '$phone' where id = $id" ;
+    $exec1 = mysqli_query($con, $query1) or die(mysqli_error($con));
+    if($exec1)
+    {
+        header("location: index.php");
 
+    }
+}
+else{
+    echo 'fill the enteries';
 }
 
 
