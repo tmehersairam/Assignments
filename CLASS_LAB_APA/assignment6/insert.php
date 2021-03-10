eb1ec98e776ef8f1734f1d5fcab7a133790ac5cd
tmehersairam <80393781+tmehersairam@users.noreply.github.com>
2021-03-10 16:27:59 +0530

    Update insert.php

--- CLASS_LAB_APA/assignment6/insert.php
+++ CLASS_LAB_APA/assignment6/insert.php
@@ -6,7 +6,7 @@ include("../assignment5/config.php");
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $phone = $_POST['phone'];
-if($first_name !== '' || $last_name !== '')
+if($first_name !== '' || $last_name !== '' || $phone !== '')
 {
     $query = "INSERT INTO STUDENTS (first_name, last_name, phone) values('$first_name','$last_name','$phone')";
     $exec = mysqli_query($con, $query) or die(mysqli_error($con));
