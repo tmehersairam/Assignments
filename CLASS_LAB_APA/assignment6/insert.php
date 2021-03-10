148bcd0c35828df5dafad1c3c7927067a6eacbca
tmehersairam <80393781+tmehersairam@users.noreply.github.com>
2021-03-10 16:25:42 +0530

    added some comments

--- CLASS_LAB_APA/assignment6/insert.php
+++ CLASS_LAB_APA/assignment6/insert.php
@@ -1,15 +1,21 @@
 <?php
 
+// config file from the assignment 5
 include("../assignment5/config.php");
+
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $phone = $_POST['phone'];
-
-$query = "INSERT INTO STUDENTS (first_name, last_name, phone) values('$first_name','$last_name','$phone')";
-
-$exec = mysqli_query($con, $query) or die(mysqli_error($con));
-if($exec )
+if($first_name !== '' || $last_name !== '')
 {
-    header("location: index.php");
+    $query = "INSERT INTO STUDENTS (first_name, last_name, phone) values('$first_name','$last_name','$phone')";
+    $exec = mysqli_query($con, $query) or die(mysqli_error($con));
+    if($exec )
+    {
+        header("location: index.php");
+    }
+}
+else{
+    echo 'Please fill the enteries';
 }
 ?>
